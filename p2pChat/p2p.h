31e03e294e2aff24aefa8c29ed98350f5fbe17ee
MonkeInBOX <judegilchrist2006@gmail.com>
2025-09-20 20:00:29 +0100

    Framework for WAN

--- p2pChat/p2p.h
+++ p2pChat/p2p.h
@@ -6,6 +6,7 @@
 using namespace std;
 
 int runClient(string Username, string ip, int port);
-int runServer(string Username, int port);
+int runServer(string Username, string ip, int port);
 void receiveMessages(SOCKET sock, string connectedUsername);
 string usernameExchange(SOCKET sock, string Username);
+string getIP();
